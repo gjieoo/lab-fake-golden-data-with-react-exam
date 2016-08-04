075af0d1a8484451620f627712da00997dbd7ab1
gjieoo <15202979397@163.com>
2016-08-04 09:52:05 +0800

    it is work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-// TODO
+const App=React.createClass({
+    render:function(){
+       return (
+         <div>
+             it's work
+         </div>
+       );
+
+    }
+});
+
+ReactDOM.render(<App/>,document.getElementById('content'));
